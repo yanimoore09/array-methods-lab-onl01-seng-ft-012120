51e86f84368ff9ddcf6bbc7efff7583752e764dd
Yani Moore <yanimoore09@gmail.com>
2020-01-23 08:59:15 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,3 +21,34 @@ end
 def using_size(array)
 
 end
+
+famous_people = ["kim jong , yani moore , brent woods , "]
+
+famous_people << "kim jong" 
+end
+famous_people push ("kim jong") 
+end
+famous_people unshift ("kim jong") 
+end
+famous_people .pop ("kim jong") 
+end
+famous_people .shift ("yani moore") 
+end
+famous_people [1] or [3]
+end
+famous_people 
+array.include ("yani moore")
+end
+
+famous_people 
+array. sort ("kim jong")
+end
+famous_people 
+array reverse ("yani moore")
+end
+
+famous_people 
+array .first ("brent woods")
+end
+
+
